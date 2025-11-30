69d76b369c02f82bde52282fd19988417261e389
seannichols1 <sean@snichols.co.nz>
2025-11-30 14:45:29 +1300

    Serious Stuff

--- snichols1/src/Nav.jsx
+++
@@ -1,9 +0,0 @@
-// import { useState, useEffect } from 'react';
-
-const navigation = () => {
-    return (
-        <div style={{backgroundColor:"Purple"}}><p>Some text Returned from Nav.JSX</p></div>
-    )
-}
-
-export default navigation;
